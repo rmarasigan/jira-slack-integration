289bf0710a27221e007a099aca183fbd62744f6c
rmarasigan <russianhielle@gmail.com>
2023-07-15 17:36:06 +0800

    MOD: remove Scope in IssueType schema

--- internal/app/jira/issue_type_schema.go
+++ internal/app/jira/issue_type_schema.go
@@ -6,14 +6,4 @@ type IssueType struct {
 	Name        string `json:"name"`        // The name of the issue type.
 	Subtask     bool   `json:"subtask"`     // Whether the issue type is used to create subtasks.
 	Description string `json:"description"` // The description of the issue type.
-	Scope       Scope  `json:"scope,omitempty"`
-}
-
-// Scope is the details of the next-gen projects the issue
-// type is available in.
-type Scope struct {
-	Type    string `json:"type,omitempty"` // The type of scope (valid values: PROJECT, TEMPLATE)
-	Project struct {
-		ID string `json:"id,omitempty"` // The ID of the project.
-	} `json:"project,omitempty"` // The project the item has scope in.
 }
